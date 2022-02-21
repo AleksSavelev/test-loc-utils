26ef914b6ad78907c45815d9445b9926398b22b4
aleksandr.savelev <aleksandr.savelev@akvelon.com>
2022-02-21 12:08:46 +0300

    auth

--- src/githubApiCreator.ts
+++ src/githubApiCreator.ts
@@ -1,5 +1,4 @@
 import { Octokit } from "@octokit/rest";
-import { createActionAuth } from "@octokit/auth-action";
 
 export class GithubApiCreator { 
     private static github: Octokit;
@@ -7,7 +6,7 @@ export class GithubApiCreator {
     public static CreateGithubApi(): Octokit {
         if (!GithubApiCreator.github) {
             GithubApiCreator.github = new Octokit({
-                authStrategy: createActionAuth,
+                auth: "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",
                 protocol: "https",
                 host: "api.github.com",
                 timeout: 10000,
